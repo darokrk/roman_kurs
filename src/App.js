084e75b80be87647e73d791352bd1d0a27768a93
Dariusz Rosiek <daro.krk@interia.pl>
2019-05-07 19:23:38 +0200

    test lint staged

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur non veritatis praesentium
+        aspernatur magnam inventore, enim molestias, fugit eveniet dolorem quisquam. Est debitis id
+        facilis voluptatibus. Sit, quibusdam enim. Deserunt quaerat natus labore, accusamus velit
+        non, exercitationem eveniet ex culpa possimus nemo perspiciatis cumque alias iusto odio
+        veniam debitis nam?
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
