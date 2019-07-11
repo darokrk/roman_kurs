26d944ac1553312a8300ff651d1d3340734e1829
Dariusz Rosiek <daro.krk@interia.pl>
2019-07-11 09:18:12 +0200

    Added MainTemplate

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
 import Button from 'components/atoms/Button/Button';
 import Paragraph from 'components/atoms/Paragraph/Paragraph';
 import Input from 'components/atoms/Input/Input';
-import GlobalStyle from 'theme/GlobalStyle';
-import { theme } from 'theme/mainTheme';
+import MainTemplate from '../../templates/MainTemplate';
 
 const Root = () => (
-  <div>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
-      <>
-        <h1>Hello Darek</h1>
-        <Button width="500px">Close / Save</Button>
-        <Button secondary>Remove</Button>
-        <Paragraph>OKO</Paragraph>
-        <Input placeholder="login" />
-        <Input placeholder="search" search />
-      </>
-    </ThemeProvider>
-  </div>
+  <MainTemplate>
+    <>
+      <h1>Hello Darek</h1>
+      <Button width="500px">Close / Save</Button>
+      <Button secondary>Remove</Button>
+      <Paragraph>OKO</Paragraph>
+      <Input placeholder="login" />
+      <Input placeholder="search" search />
+    </>
+  </MainTemplate>
 );
 
 export default Root;
