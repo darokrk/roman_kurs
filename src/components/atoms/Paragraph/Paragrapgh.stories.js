c325946506171ae4442540112bc2bcc5ac500c09
Dariusz Rosiek <daro.krk@interia.pl>
2019-07-11 09:48:28 +0200

    Add Atoms folder to storybook

--- src/components/atoms/Paragraph/Paragrapgh.stories.js
+++ src/components/atoms/Paragraph/Paragrapgh.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Paragraph from './Paragraph';
 
-storiesOf('Paragraph', module)
+storiesOf('Atoms/Paragraph', module)
   .add('normal', () => <Paragraph>Hello Darek</Paragraph>)
   .add('large', () => <Paragraph large>Hello Darek</Paragraph>)
   .add('big', () => <Paragraph big>Hello Darek</Paragraph>);
